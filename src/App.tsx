b56a070c0d86789455b700e3911e6bb2727c191c
Augusto Sardinha <ssardinhaaugusto@gmail.com>
2022-03-29 11:40:38 -0300

    Project Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Header } from './components/Header';
+import { Home } from './pages/Home/Home';
+import { UserTodo } from './pages/UserTodo/UserTodo';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" >
+          <Route path=":id" element={<UserTodo />} />
+        </Route>
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
